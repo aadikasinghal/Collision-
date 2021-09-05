2a97933ddb104ba6f9be98311702a4973a7e2bea
aadikasinghal <82499672+aadikasinghal@users.noreply.github.com>
2021-09-05 14:02:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var fixedRect;
 var movingRect;
+var box3;
  
 function setup() {
   createCanvas(800,400);
@@ -9,6 +10,9 @@ function setup() {
   fixedRect= createSprite(200, 200, 80, 30);
   fixedRect.shapeColor="red";
   fixedRect.debug="true";
+  box3= createSprite(300,200,70,40);
+  box3.shapeColor="pink";
+  box3.debug="true";
 }
 
 function draw() {
@@ -17,18 +21,24 @@ function draw() {
   movingRect.x=World.mouseX;
   movingRect.y=World.mouseY;
 
-  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
-      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 )
-      {
-        movingRect.shapeColor="green";
-        fixedRect.shapeColor="red";
-      }
-   else{
-        movingRect.shapeColor="yellow";
-        fixedRect.shapeColor="yellow";
-   }   
+  black(movingRect, fixedRect);
+  black(box3, movingRect);
 
   drawSprites();
+
+}
+
+function black(box1 ,box2){
+  if(box1.x - box2.x < box2.width/2 + box1.width/2 
+    && box2.x - box1.x < box2.width/2 + box1.width/2
+     && box1.y - box2.y < box2.height/2 + box1.height/2
+     && box1.y - box2.y < box2.height/2 + box1.height/2 )
+     {
+       box1.shapeColor="green";
+       box2.shapeColor="red";
+     }
+  else{
+       box1.shapeColor="yellow";
+       box2.shapeColor="yellow";
+  }   
 }
